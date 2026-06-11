7355b05334028beaeca37e4f7464434cd9d35a4a
dudatonini <eduardatonfer@gmail.com>
2026-06-10 23:06:10 -0300

    comecando interface web

---
+++ interface_web/app/components/header/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+        <div className="text-6xl font-extrabold text-violet-900 font-sans">
+            <h1> Teclado Laser </h1>
+        </div>
+    )
+}
---
+++ interface_web/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex flex-col items-center bg-violet-200 pt-12">
+      
+      <Header/>
+      
+    </div>
+  );
+}
